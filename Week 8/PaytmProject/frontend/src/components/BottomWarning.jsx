cc546a12c6b039d5ea0a45eb1182b02162dd9d58
Goutam Choudhary <goutamchoudhary907@gmail.com>
2025-01-04 18:59:37 +0530

    Add All components of interface

---
+++ Week 8/PaytmProject/frontend/src/components/BottomWarning.jsx
@@ -0,0 +1,10 @@
+export function BottomWarning({label, buttontext,to}){
+  return <div className="py-2 text-sm flex justify-center">
+    <div>
+        {label}
+    </div>
+    <Link className="pointer underline pl-1 cursor-pointer" to={to}>
+    {buttontext}
+    </Link>
+  </div>
+}
