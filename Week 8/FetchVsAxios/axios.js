e19f90a1d4aece9c06b4b2aaff763ddbf5e4e7fb
Goutam Choudhary <goutamchoudhary907@gmail.com>
2025-01-05 22:42:41 +0530

    update

--- Week 8/FetchVsAxios/axios.js
+++ Week 8/FetchVsAxios/axios.js
@@ -9,7 +9,16 @@ async function main(){
 
 // only specify request type in axios. and no need to specify data type 
 async function postMethod(){
-    const response=await axios.post("https://www.postb.in/1735836174228-5991195125970")
+    const response=await axios.post("https://www.postb.in/1735836174228-5991195125970" ,
+        {
+            username:"Goutam",          //way to send body in request
+        },
+        {
+            headers:{
+                Authorization:"Bearer 123"           // way to send headers 
+            }
+        }
+    )
     console.log(response.data);
     
 }
