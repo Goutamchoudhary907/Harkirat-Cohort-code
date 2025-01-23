6ed3ec394bbb4877510ce9a7fe0f554c54fa5177
Goutam Choudhary <goutamchoudhary907@gmail.com>
2025-01-23 22:59:03 +0530

    Medium project

--- Week 13/MediumProject/backend/src/index.ts
+++ Week 13/MediumProject/backend/src/index.ts
@@ -1,13 +1,67 @@
 import { Hono } from 'hono'
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+import { decode, sign,verify } from 'hono/jwt'
+const app = new Hono<{
+// Giving type to databse url globally 
+  Bindings:{
+    DATABASE_URL:string;
+    JWT_SECRET:string
+  }
+}>();
 
-const app = new Hono()
+// Middleware
+app.use('/api/v1/blog/*', async (c,next) =>{
+  const header=c.req.header("Authorization") || "";
 
-app.post('/api/v1/signup', (c) => {
-  return c.text('Hello Hono!')
+  const response=await verify(header, c.env.JWT_SECRET)
+  if(response.id){
+    next()
+  }else{
+    c.status(403)
+    return c.json({error:"unathorized"})
+  }
 })
 
-app.post('/api/v1/signin', (c) => {
-  return c.text('Hello Hono!')
+app.post('/api/v1/signup', async (c) => {
+  const prisma =new PrismaClient({
+  datasourceUrl:c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  const body=await c.req.json();
+ const user= await prisma.user.create({
+    data:{
+      email:body.email,
+      password:body.password,
+    }
+  })
+
+  const token=await sign({id:user.id},c.env.JWT_SECRET)
+  return c.json({
+    jwt:token
+  })
+})
+
+app.post('/api/v1/signin',async (c) => {
+ const prisma=new PrismaClient({
+  datasourceUrl:c.env?.DATABASE_URL,
+ }).$extends(withAccelerate());
+
+ const body=await c.req.json();
+ const user=await prisma.user.findUnique({
+  where:{
+    email:body.email ,
+    password:body.password
+  }
+ });
+
+ if(!user){
+  c.status(403);
+  return c.json({error:"user not found"})
+ }
+
+ const jwt=await sign({id:user.id},c.env.JWT_SECRET);
+ return c.json({jwt});
 })
 
 app.post('/api/v1/blog', (c) => {
