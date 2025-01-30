e5d914dc6b60a632b348a8793ca69ce200d7daaf
Goutam Choudhary <goutamchoudhary907@gmail.com>
2025-01-30 23:44:57 +0530

    Add next js

--- Week-14/next-backend/app/api/user/route.ts
+++ Week-14/next-backend/app/api/user/route.ts
@@ -1,11 +1,20 @@
 //  /api/user is the route 
 
 import { NextRequest } from "next/server";
+import { PrismaClient } from "@prisma/client";
+import { log } from "console";
+const client=new PrismaClient();
 
 export async function POST(req:NextRequest){
     const body=await req.json();
-    console.log(body);
-    
+   console.log(body);
+   
+   await client.user.create({
+        data:{
+            username:body.username,
+            password:body.password
+        }
+    })
     return Response.json({
      message:"You are logged in"
     })
