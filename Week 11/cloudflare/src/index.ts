bf76ce0f5ea0613090d06d101af1fd67627337b9
Goutam Choudhary <goutamchoudhary907@gmail.com>
2025-01-31 22:53:09 +0530

    update

--- Week 11/cloudflare/src/index.ts
+++
@@ -1,8 +0,0 @@
-export default {
-	async fetch(request, env, ctx): Promise<Response> {
-			return Response.json({
-				message:"You did npt send a get request"
-			})
-		
-	},
-} satisfies ExportedHandler<Env>;
